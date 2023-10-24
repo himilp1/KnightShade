39585503b9d9cf42bd2ff8f203788bcaf3ac6e30
Grant <granthendrickson@knights.ucf.edu>
2023-10-24 15:17:27 -0400

    Basic health regeneration

--- Assets/scripts/playerCharacterScripts/PlayerHealth.cs
+++ Assets/scripts/playerCharacterScripts/PlayerHealth.cs
@@ -5,25 +5,55 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+    public int regenRate = 1; // Health regen per second
+    public float regenDelay = 5f; // Time without damage before regen starts
 
     public Animator animator;
     public HealthBar healthBar;
+
+    private bool canRegenerate;
+    private float lastDamageTime;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         animator = GetComponent<Animator>();
+        canRegenerate = false;
+    }
+
+    void Update()
+    {
+        if (currentHealth < maxHealth && canRegenerate)
+        {
+            RegenerateHealth();
+        }
     }
 
     public void TakeDamage(int damage)
     {
-        currentHealth = currentHealth - damage;
+        currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+
+        lastDamageTime = Time.time; // Record the time of the last damage
 
         if (currentHealth <= 0)
         {
             playerIsDead();
+        }
+        else
+        {
+            canRegenerate = true; // The player can start regenerating health
+        }
+    }
+
+    void RegenerateHealth()
+    {
+        if (Time.time - lastDamageTime > regenDelay)
+        {
+            // Eventually change to use regen rate to regenerate over time rather than instantly
+            currentHealth = 100;
+            healthBar.SetHealth(currentHealth);
         }
     }
 
