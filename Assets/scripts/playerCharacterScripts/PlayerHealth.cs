23b0abd5f9e9c31e0df435deaaed89f9d79e3f51
Grant <granthendrickson@knights.ucf.edu>
2023-10-24 14:54:47 -0400

    Health Bar UI Working

--- Assets/scripts/playerCharacterScripts/PlayerHealth.cs
+++ Assets/scripts/playerCharacterScripts/PlayerHealth.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour{
+public class PlayerHealth : MonoBehaviour
+{
     public int maxHealth;
     public int currentHealth;
+
     public Animator animator;
-    void Start(){
+    public HealthBar healthBar;
+
+    void Start()
+    {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
         animator = GetComponent<Animator>();
     }
 
-    public void TakeDamage(int damage){
+    public void TakeDamage(int damage)
+    {
         currentHealth = currentHealth - damage;
-        if(currentHealth <= 0){
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
             playerIsDead();
         }
     }
 
-    public void playerIsDead(){
+    public void playerIsDead()
+    {
         GetComponent<CharacterController>().enabled = false;
         GetComponent<PlayerAttack>().enabled = false;
         GetComponent<ThirdPersonPlayer>().enabled = false;
