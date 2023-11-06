4fdc4b918166da14e57b09933017f6c0046a7aaf
Grant <granthendrickson@knights.ucf.edu>
2023-11-06 16:27:28 -0500

    Basic Weapon chest game object (non-functional)

---
+++ Assets/MysteryBox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryBox : MonoBehaviour
+{
+    public void Open()
+    {
+        // Open Box
+        Debug.Log("Opened Chest");
+    }
+}
