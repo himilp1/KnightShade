997035dbb9a2ba92359b25ebc27ca8987f6412b7
Grant <granthendrickson@knights.ucf.edu>
2023-11-26 22:53:27 -0500

    Castle Gate Noise

--- Assets/scripts/map/CastleGate.cs
+++ Assets/scripts/map/CastleGate.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class CastleGate : MonoBehaviour
 {
     public float raiseAmount = 2.0f; // Adjust this value to control how much the gate raises.
+    public AudioSource openSound;
 
     public void Open()
     {
         // Raise the CastleGate on the Y-axis
         Vector3 newPosition = transform.position + Vector3.up * raiseAmount;
         transform.position = newPosition;
+
+        openSound.Play();
 
         Debug.Log("Opened Gate");
     }
